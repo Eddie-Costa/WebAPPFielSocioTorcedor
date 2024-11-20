8ce08cc0f09eb5790ca8d5b9da376896ca34a142
Eddie Alencar Costa <eddiealencarcosta@gmail.com>
2024-11-20 15:32:03 -0300

    Update Db.sql

--- Db.sql
+++ Db.sql
@@ -6,8 +6,8 @@ Create table Socio(
 	Id_socio int Primary Key auto_increment not null,
     Nome varchar(60) not null,
     Data_nasc date not null,
-    Cpf int not null,
-    Telefone varchar(12) not null,
+    Cpf varchar(14) not null,
+    Telefone varchar(15) not null,
     Email varchar(60) not null,
     Senha varchar(30) not null,
     Plano int not null comment '1 - Minha Vida | 2 - Minha Historia | 3 - Meu Amor'
